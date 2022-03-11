0a4ec76ca33cb18e5d29802541d638b9bcc21f8a
stels54 <stels54@gmail.com>
2022-03-11 14:23:34 +0300

    check free rooms

--- repositories/BookingRepository.php
+++ repositories/BookingRepository.php
@@ -6,6 +6,26 @@ use app\entities\Booking;
 
 class BookingRepository
 {
+    public function getCountByCategoryAndDates($categoryId, $dateFrom, $dateTo)
+    {
+        return Booking::find()
+            ->where(['room_category_id' => $categoryId])
+            ->andWhere([
+                'or',
+                [
+                    'and',
+                    ['<=', 'date_from', $dateFrom],
+                    ['>=', 'date_to',  $dateFrom]
+                ],
+                [
+                    'and',
+                    ['<=', 'date_from', $dateTo],
+                    ['>=', 'date_to',  $dateTo]
+                ]
+            ])
+            ->count();
+    }
+
     public function save(Booking $booking): void
     {
         if (!$booking->save(false)) {
--- services/BookingService.php
+++ services/BookingService.php
@@ -5,18 +5,28 @@ namespace app\services;
 use app\forms\BookingForm;
 use app\repositories\BookingRepository;
 use app\entities\Booking;
+use app\repositories\RoomCategoryRepository;
 
 class BookingService
 {
     private $bookings;
+    private $roomCategories;
 
-    public function __construct(BookingRepository $bookings)
+    public function __construct(BookingRepository $bookings, RoomCategoryRepository $roomCategories)
     {
         $this->bookings = $bookings;
+        $this->roomCategories = $roomCategories;
     }
 
     public function create(int $categoryId, BookingForm $form) : void
     {
+        $roomCategory = $this->roomCategories->get($categoryId);
+        $bookingsCount = $this->bookings->getCountByCategoryAndDates($categoryId, $form->date_from, $form->date_to);
+
+        if ($roomCategory->rooms_count <= $bookingsCount) {
+            throw new \DomainException('All rooms are booked');
+        }
+
         $booking = Booking::create(
             $categoryId,
             $form->date_from,
