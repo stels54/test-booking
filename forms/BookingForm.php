1b313c1f2d688061883ff96027c9136eca9562f6
stels54 <stels54@gmail.com>
2022-03-11 14:21:49 +0300

    date interval validator

--- forms/BookingForm.php
+++ forms/BookingForm.php
@@ -2,6 +2,7 @@
 
 namespace app\forms;
 
+use app\helpers\DatetimeHelper;
 use yii\base\Model;
 
 class BookingForm extends Model
@@ -20,6 +21,14 @@ class BookingForm extends Model
             [['date_from', 'date_to', 'customer_name', 'customer_email'], 'required'],
             [['date_from', 'date_to'], 'safe'],
             [['customer_name', 'customer_email'], 'string', 'max' => 255],
+            ['date_from', 'validateDates']
         ];
+    }
+
+    public function validateDates($attribute, $params)
+    {
+        if (!DatetimeHelper::validateDateInterval($this->date_from, $this->date_to)) {
+            $this->addError($attribute, 'Interval is not correct');
+        }
     }
 }
